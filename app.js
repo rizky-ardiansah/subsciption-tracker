75d07700a2f18369127a53bbe7efbbe6c496f329
Muhammad Rizky Ardiansah <rizkyardiansah.dev@gmail.com>
2025-03-17 11:24:48 +0700

    add error handling middleware

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ import subscriptionRouter from './routes/subscription.routes.js';
 
 import { PORT } from './config/env.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser())
 
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/posts", subscriptionRouter)
+
+app.use(errorMiddleware)
 
 app.get('/', (req, res) => {
     res.send('Hello World');
